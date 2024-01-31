cd24c7e6164e9ae28bdf9db2d01df8aac5da2944
sir <gari69wat@gmail.com>
2024-01-31 12:19:30 +0000

    rec-6 //inher-5

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
