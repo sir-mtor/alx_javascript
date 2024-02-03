6dfbfbd16a131de959fd7287f057fb8ffb59984a
sir <gari69wat@gmail.com>
2024-02-03 15:20:29 +0000

    stars

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,9 +1,12 @@
 const request = require('request');
 
+// extract
 const apiUrl = process.argv[2];
 
+// character ID
 const characterId = '18';
 
+//make the guest request
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
@@ -11,9 +14,12 @@ request.get(apiUrl, (error, response, body) => {
     console.error('Failed to fetch data. Status code:', response.statusCode);
   } else {
     
+    // parse
     const filmsData = JSON.parse(body);
     
+    //count
     const count = filmsData.results.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+    // print count
     console.log(count);
   }
 });
