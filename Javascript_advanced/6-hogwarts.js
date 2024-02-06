42de9c16f438a2fb27d0c6bddc0e0121adf69063
sir <gari69wat@gmail.com>
2024-02-06 15:51:52 +0000

    mode

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,39 @@
+var studentHogwarts = (function() {
+    var privateScore = 0;
+    var name = null;
+
+    function changeScoreBy(points) {
+        privateScore += points;
+    }
+
+    return {
+        setName: function(newName) {
+            name = newName;
+        },
+        rewardStudent: function() {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function() {
+            changeScoreBy(-1);
+        },
+        getScore: function() {
+            return name + ': ' + privateScore;
+        }
+    };
+})();
+
+var harry = Object.create(studentHogwarts);
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+var draco = Object.create(studentHogwarts);
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
