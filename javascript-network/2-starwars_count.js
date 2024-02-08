e14bef3da22ebc2afc852fb592e01afe1e0247c5
sir <gari69wat@gmail.com>
2024-02-08 18:03:57 +0000

    star

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,25 +1,32 @@
 const request = require('request');
 
-// extract
-const apiUrl = process.argv[2];
+const apiURL = process.argv[2];
 
-// character ID
-const characterId = '18';
+if (!apiURL) {
+  console.error('Please provide the API URL as an argument.');
+  process.exit(1);
+}
 
-//make the guest request
-request.get(apiUrl, (error, response, body) => {
+request(apiURL, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-  } else {
-    
-    // parse
-    const filmsData = JSON.parse(body);
-    
-    //count
-    const count = filmsData.results.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    // print count
-    console.log(count);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  let wedgeFilms = 0;
+
+  data.results.forEach(film => {
+    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      wedgeFilms++;
+    }
+  });
+
+  console.log(wedgeFilms);
 });
+
